70f97d48541a80bd211b7d10ec55fa913b20aab2
Kumar, Sanjay (623) <sanjay.kumar@mercedes-benz.com>
2023-04-04 07:24:14 +0530

    first commit

---
+++ BTsocket/BTsocket/demo.swift
@@ -0,0 +1,43 @@
+//
+//  demo.swift
+//  BTsocket
+//
+//  Created by Kumar, Sanjay on 04/04/23.
+//
+
+import Foundation
+import CoreBluetooth
+
+public protocol SocketService {
+    associatedtype T
+    var socketChannel: T? { get set }
+    func create(channel: T)
+    
+}
+
+public class BTSocketHandler: SocketService {
+    public typealias T = CBL2CAPChannel
+    public var socketChannel: CBL2CAPChannel?
+    public func create(channel: T) {}
+}
+
+public class BTManager<U: SocketService>: NSObject {
+    private var centralManager: CBCentralManager
+    public var socketServices: any SocketService
+    
+    public init(socketServices: any SocketService) {
+        self.socketServices = socketServices
+        self.centralManager = .init()
+    }
+}
+
+extension BTManager : CBPeripheralDelegate {
+    public func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+    }
+    
+    public func peripheral(_ peripheral: CBPeripheral, didOpen channel: CBL2CAPChannel?, error: Error?) {
+        guard let channel = channel else { return }
+        socketServices.create(channel: channel)
+    }
+    
+}
